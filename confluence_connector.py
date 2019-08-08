00debcb9e108b3819b2da43d8aeba3148262ad65
cwadhwani <cwadhwani@splunk.com>
2019-08-08 15:18:21 +0530

    Confluence: Adding missing data paths

--- confluence_connector.py
+++ confluence_connector.py
@@ -366,8 +366,9 @@ if __name__ == '__main__':
 
     if (username and password):
         try:
+            login_url = BaseConnector._get_phantom_base_url() + "login"
             print ("Accessing the Login page")
-            r = requests.get("https://127.0.0.1/login", verify=False)
+            r = requests.get(login_url, verify=False)
             csrftoken = r.cookies['csrftoken']
 
             data = dict()
@@ -377,10 +378,10 @@ if __name__ == '__main__':
 
             headers = dict()
             headers['Cookie'] = 'csrftoken=' + csrftoken
-            headers['Referer'] = 'https://127.0.0.1/login'
+            headers['Referer'] = login_url
 
             print ("Logging into Platform to get the session id")
-            r2 = requests.post("https://127.0.0.1/login", verify=False, data=data, headers=headers)
+            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print ("Unable to get session id from the platfrom. Error: " + str(e))
